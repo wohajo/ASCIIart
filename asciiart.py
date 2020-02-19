bb8357b41b69dc8f11bcb28ffc1fc5e1b11d86bd
wohajo <michalos222@gmail.com>
2020-02-19 15:11:37 +0100

    added reading size

--- asciiart.py
+++ asciiart.py
@@ -1,3 +1,16 @@
-import Image from PIL
+from PIL import Image
+
+def convertIntTuple(tup):
+    str = ''.join(tup)
+    return str
 
 print ("welcome to ASCII art converter")
+
+image1 = Image.open("parrot.jpg")
+height, width = image1.size
+
+pixels = []
+
+for i in range(width):
+    for j in range(height):
+        pixels.append(image1[i, j] + (i, j))
