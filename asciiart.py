b1a1f0a0dc04da892eacc97fab4a0e119dbfab4b
wohajo <michalos222@gmail.com>
2020-02-20 18:44:07 +0100

    test changes

--- asciiart.py
+++ asciiart.py
@@ -25,8 +25,13 @@ if __name__ == "__main__":
     for i in range(height):
         print()
         for j in range(width):
-            print("." if pixels[j][i] > 127 else ":", end="")
-
+            pixel = pixels[j][i]
+            if pixel >= 170:
+                print(chr(254), end="")
+            elif pixel >= 85 and pixel < 170: 
+                print(":", end="")
+            elif pixel < 85:
+                print(".", end="")
     # Average: average the R, G and B values - (R + G + B) / 3
     # Lightness: average the maximum and minimum values out of R, G and B - max(R, G, B) + min(R, G, B) / 2
     # Luminosity: take a weighted average of the R, G and B values to account for human perception - 0.21 R + 0.72 G + 0.07 B
