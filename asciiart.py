ab3305077237b7ea1615de7f6029fe71d88b6723
wohajo <michalos222@gmail.com>
2020-02-23 10:09:00 +0100

    added different modes of calculating rgb value

--- asciiart.py
+++ asciiart.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-def loadImage(imagePath):
+def loadImage(imagePath, selection):
 
     pixels = []
 
@@ -10,15 +10,14 @@ def loadImage(imagePath):
             templist.append(image.getpixel((i, j)))
         pixels.append(templist)
 
-    #TODO put modes here
-    for i in range(height):
-        for j in range(width):
-            r, g, b = pixels[i][j]
-            pixels[i][j] = (r + g + b) // 3 # dzielenie calkowite
+    if (selection == 3):
+        return luminosityCalculation(pixels, height, width)
+    elif (selection == 2):
+        return lightnessCalculation(pixels, height, width)
+    elif (selection == 1):
+        return averageCalculation(pixels, height, width)
 
-    return pixels
-
-def printPixels(height, width, pixels):
+def printPixels(pixels, height, width):
     for i in range(height):
         print()
         for j in range(width):
@@ -30,44 +29,61 @@ def printPixels(height, width, pixels):
             elif pixel < 85:
                 print("..", end="")
 
-def lightnessCalculation():
-    pass
+def lightnessCalculation(pixels, height, width):
+    
+    for i in range(height):
+        for j in range(width):
+            r, g, b = pixels[i][j]
+            pixels[i][j] = (r + g + b) // 3
 
-def averageCalculation():
-    pass
+    return pixels
 
-def luminosityCalculation():
-    pass
+def averageCalculation(pixels, height, width):
+    
+    for i in range(height):
+        for j in range(width):
+            r, g, b = pixels[i][j]
+            pixels[i][j] = (max(r, g, b) + min(r, g, b)) // 2
+
+    return pixels
+
+def luminosityCalculation(pixels, height, width):
+    
+    for i in range(height):
+        for j in range(width):
+            r, g, b = pixels[i][j]
+            pixels[i][j] = int(0.21 * r + 0.72 * g + 0.07 * b)
+
+    return pixels
 
 if __name__ == "__main__":
     
     # getting file
-    print ("welcome to ASCII art converter")
-    print ("provide a filename: ")
+    print ("Welcome to ASCII art converter")
+    print ("Provide a filename: ")
     filename = input()
+
+    image = Image.open(filename)
+    height, width = image.size
+    print("Image loaded! Size: ", height, "x", width)
     #TODO secure input for jpgs only
     #TODO chcecking if file is in folder?
 
-    print ("select one of the modes: ")
+    # selecting mode
+    print ("Select one of the modes: ")
     print ("1: Average")
     print ("2: Lightness")
     print ("3: Luminosity")
     selection = int(input("Select mode: "))
+
     while (selection > 3 or selection < 1):
         selection = int(input("Select mode (1 - 3): "))
-        #TODO selection of calculations
+        #TODO secure input from other chars
 
-    image = Image.open(filename)
-    height, width = image.size
-    print("image size: ", height, "x", width)
+    pixels = loadImage(filename, selection)
+    printPixels(pixels, height, width)
 
-    pixels = loadImage((filename))
-    printPixels(height, width, pixels)
-
-    print("")
-    # Average: average the R, G and B values - (R + G + B) / 3
-    # Lightness: average the maximum and minimum values out of R, G and B - max(R, G, B) + min(R, G, B) / 2
-    # Luminosity: take a weighted average of the R, G and B values to account for human perception - 0.21 R + 0.72 G + 0.07 B
+    print("\n\n")
 
     #robi cos w jednej linijce i iteruje po wszystkim
     # i = [1, 2, 3]
